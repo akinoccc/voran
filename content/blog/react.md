a412ddc02856524397f19ff6e9d1626b6dcbbaac
akinocccc <akinochen@foxmail.com>
2024-06-05 20:22:17 +0800

    wip(blog): blog list

---
+++ content/blog/react.md
@@ -0,0 +1,5 @@
+---
+title: 'React'
+description: 'meta description of the page'
+date: 2020-01-01
+---
---
+++ pages/blog.vue
@@ -0,0 +1,15 @@
+<template>
+    <main class="p-16">
+        <ContentList path="/blog" v-slot="{ list }">
+            <div class="mb-4" v-for="blog in list" :key="blog._path">
+                <span>{{ blog.title }}</span>
+                <span class="ml-2 text-neutral text-size-xs">{{ blog.date }}</span>
+                <!-- <span>{{ blog.description }}</span> -->
+            </div>
+        </ContentList>
+    </main>
+</template>
+
+<script setup lang="ts">
+
+</script>
