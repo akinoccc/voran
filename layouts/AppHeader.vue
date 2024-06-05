814e0be39117abad15c7edf66aa185c51a186cf6
akinocccc <akinochen@foxmail.com>
2024-06-05 20:20:48 +0800

    feat(layouts): add header

---
+++ layouts/AppHeader.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="flex flex-justify-between">
+        <div class="flex">
+            <div class="mr-4">
+                <div class="">logo</div>
+            </div>
+        </div>
+        <div class="flex flex-1 flex-justify-end text-neutral text-size-sm font-400">
+            <a class="decoration-none hover:text-black" href="/blog">
+                Blog
+            </a>
+            <a class="ml-4 decoration-none hover:text-black">
+                Projects
+            </a>
+            <a class="ml-4 hover:text-black i-tabler-brand-discord" href="https://x.com/akinoccc" target="_blank" />
+            <a class="ml-4 hover:text-black i-tabler-brand-x" href="https://x.com/akinoccc" target="_blank" />
+            <a class="ml-4 hover:text-black i-tabler-brand-github" href="https://github.com/akinocccc"
+                target="_blank" />
+        </div>
+    </div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="p-8 pl-14 pr-14">
+        <AppHeader />
+        <slot />
+    </div>
+</template>
+
+<script setup lang="ts">
+import AppHeader from "./AppHeader.vue";
+</script>
