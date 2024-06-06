dd554d448f6059506584e90f3ff8cef4d292cde9
Akino <akinochen@foxmail.com>
2024-06-06 17:46:14 +0800

    feat(blog): add blog detail

---
+++ pages/articles/[id].vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import dayjs from 'dayjs'
+</script>
+
+<template>
+  <main class="px-7 py-10">
+    <div class="m-auto" style="max-width: 67ch;">
+      <ContentDoc v-slot="{ doc }" tag="div">
+        <ContentRenderer :value="doc">
+          <h1>{{ doc.title }}</h1>
+          <span class="text-neutral">{{ dayjs(doc.date).format("MMM D") }}</span>
+          <ContentRendererMarkdown tag="article" :value="doc" />
+        </ContentRenderer>
+      </ContentDoc>
+    </div>
+  </main>
+</template>
